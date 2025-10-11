ba964a53428890d64792f211d995c0bf492f4e38
Aakash Jyoti Srivastava <144765366+aakash8409@users.noreply.github.com>
2025-10-11 04:15:05 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144543
+let accountEmail = "aakash@gmail.com"
+var accountPassword = "123456" // not used b 
+accountCity = "Bhagalpur"
+
+// accountId = 2 //Not allowed
+accountEmail = "hchc@gmal.com"
+accountPassword = "234213r135"
+accountCity = "patna"
+let accountState;
+
+console.log(accountId)
+
+/*
+Prefer not to use war
+because of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+//alert( 3 + 3) //we are using nodeJS, not browser
+
+console.log(3 + 3); console.log("Aakash") //code redability should be high
+
+let name = "Aakash"
+let Age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint only use for metadata
+// string => ""
+// boolean => true/fasle
+// null => standalone value
+// undefine => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined)
