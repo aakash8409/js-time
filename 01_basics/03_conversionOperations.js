c434b49771a94a0c96531e3844d264b0df619e0d
Aakash Jyoti Srivastava <144765366+aakash8409@users.noreply.github.com>
2025-10-14 07:22:25 +0000

    converison and comparision

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,26 +1,72 @@
-let score = "Aakash"
+// let score = "Aakash"
 
 //console.log(typeof score);
 //console.log(typeof (score));
 
-let valueInNumber = Number(score)
+// let valueInNumber = Number(score)
 //console.log(typeof valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
 // true => 1; false => 0
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
  
 // 1 => true; 0 => false
 // "" => false
 // "hitesh" => true
 
-let someNumber = 44
+// let someNumber = 44
 
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//**********************************Operations************************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = " Aakash"
+// let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" +2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+2
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+console.log(`x:${x}, y:${y}`);
+
+let a = 3;
+const b = ++a;
+console.log(`a:${a}, b:${b}`);
+
 
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,8 @@
+// console.log(2 > 1);
+console.log("2" > 1);
+
+
+// The reason is that an equality check == and comparisons > < >= <= works differently.
+// Comparision convert null to a Number, treating it as 0.
+// Thats why (3) null >= 0 is true and (1)null > 0 is false.
+
